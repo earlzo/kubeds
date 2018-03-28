de30cabe92770f0501e0d1f59c711a2a8b85485c
earlzo <dev@earlzo.me>
2018-03-28 19:15:19 +0800

    Fixed: cmd/test

--- leizu/cmd/test.go
+++ leizu/cmd/test.go
@@ -2,14 +2,9 @@ package cmd
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
-	"os"
-	"os/exec"
-
 	"github.com/shanbay/leizu/test/resource"
 	"github.com/gogo/protobuf/jsonpb"
-	"github.com/golang/glog"
 	"github.com/shanbay/leizu"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -19,9 +14,7 @@ import (
 
 var (
 	xdsPort       uint
-	ads           bool
 	bootstrapFile string
-	envoyBinary   string
 )
 
 var testCmd = &cobra.Command{
@@ -29,23 +22,15 @@ var testCmd = &cobra.Command{
 	Short: "test leizu",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		kubeClient, err := leizu.SimpleKubeClient(viper.GetViper())
-		if err != nil {
-			logrus.WithError(err).Fatalln("get kube client failed")
-		}
-		ns := viper.GetString("namespace")
-		fmt.Print(kubeClient, ns)
-
 		app := leizu.InitApplication(viper.GetViper())
-		go app.Serve()
-
 		// write bootstrap file
+		ns := viper.GetString("namespace")
 		bootstrap := resource.MakeBootstrap(uint32(xdsPort), 19000)
 		services, err := app.KubeClient.CoreV1().Services(ns).List(k8sApiMetaV1.ListOptions{})
 		if err != nil {
 			logrus.Warnln(err)
 		}
-		for _, svc := range services.Items{
+		for _, svc := range services.Items {
 			clusterName := svc.Name + "." + svc.Namespace
 			cluster := resource.MakeCluster(app.Config.GetBool("ads"), clusterName)
 			bootstrap.StaticResources.Clusters = append(bootstrap.StaticResources.Clusters, *cluster)
@@ -53,24 +38,19 @@ var testCmd = &cobra.Command{
 
 		buf := &bytes.Buffer{}
 		if err := (&jsonpb.Marshaler{OrigName: true}).Marshal(buf, bootstrap); err != nil {
-			glog.Fatal(err)
+			logrus.WithError(err).Fatalln("marshal bootstrap file failed")
 		}
 		if err := ioutil.WriteFile(bootstrapFile, buf.Bytes(), 0644); err != nil {
-			glog.Fatal(err)
+			logrus.WithError(err).Fatalln("write bootstrap file failed")
 		}
+		logrus.WithField("path", bootstrapFile).Infoln("please start envoy with bootstrap file")
 
-		// start envoy
-		envoy := exec.Command("envoy", "-c", bootstrapFile, "--drain-time-s", "1")
-		envoy.Stdout = os.Stdout
-		envoy.Stderr = os.Stderr
-		envoy.Start()
+		app.Serve()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(testCmd)
 
-	testCmd.Flags().BoolVar(&ads, "ads", true, "Use ADS instead of separate xDS services")
 	testCmd.Flags().StringVar(&bootstrapFile, "bootstrap", "bootstrap.json", "Bootstrap file name")
-	testCmd.Flags().StringVar(&envoyBinary, "envoy", "envoy", "Envoy binary file")
 }
